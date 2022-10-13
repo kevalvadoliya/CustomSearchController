1fe7aea9e980f9d317991817229e93ab38e2e95d
Keval Vadoliya <keval.vadoliya@kiprosh.com>
2022-10-13 19:01:24 +0530

    Update UIView+Extension.swift

--- Sources/Extensions/UIView+Extension.swift
+++ Sources/Extensions/UIView+Extension.swift
@@ -11,7 +11,7 @@ extension UIView {
     
     func loadViewFromNib(nibName: String? = nil) -> UIView {
         let identifier: String = nibName ?? String(describing: type(of: self))
-        let nib = UINib(nibName: identifier, bundle: nil)
+        let nib = UINib(nibName: identifier, bundle: Bundle.module)
         guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
             return UIView()
         }
