28b0bbb0fffaae813e66529f8e2d2d1a0cbf883f
Keval Vadoliya <keval.vadoliya@kiprosh.com>
2022-10-13 15:47:21 +0530

    project setup

---
+++ CustomSearchControllerDemo/CustomSearchControllerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSearchControllerDemo
+//
+//  Created by Keval Vadoliya on 13/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
